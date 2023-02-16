9b797cf279e020dd1248a2a56d78843ac522ef15
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-16 22:35:56 +0000

    added better error handling

--- modules/lib/utils/array.js
+++ modules/lib/utils/array.js
@@ -31,6 +31,12 @@ export function shuffle(array) {
   return array;
 }
 
+function setErrorAndType(res, error) {
+  res.success = false;
+  res.error = error;
+  return res;
+}
+
 // (a,b)⇒{a<x<b}
 // [a,b]⇒{a..b}⇒{a≤x≤b}
 
@@ -38,54 +44,49 @@ function getIntervalInfo(/** @type {string}*/ interval) {
   interval = interval.trim();
   const first = interval[0];
   const last = interval[interval.length - 1];
-  const res = { from: 0, to: 0, success: true, includeStart: false, includeEnd: false };
+  const res = {
+    from: 0,
+    to: 0,
+    includeStart: false,
+    includeEnd: false,
+    reverse: false,
+    success: true,
+    error: "",
+  };
 
   if (first === "(") res.includeStart = false;
   else if (first === "[") res.includeStart = true;
-  else {
-    res.success = false;
-    return res;
-  }
+  else return setErrorAndType(res, "Invalid start character. Allowed characters are '(' and '['.");
 
   if (last === ")") res.includeEnd = false;
   else if (last === "]") res.includeEnd = true;
-  else {
-    res.success = false;
-    return res;
-  }
+  else return setErrorAndType(res, "Invalid end character. Allowed characters are ')' and ']'.");
 
   const usesCommaDelimiter = interval.indexOf(",") !== -1;
-  if (!usesCommaDelimiter && interval.indexOf("..") === -1) {
-    res.success = false;
-    return res;
-  }
+  if (!usesCommaDelimiter && interval.indexOf("..") === -1)
+    return setErrorAndType(res, "Invalid delimeter. Allowed characters are ',' and '..'.");
 
   const inner = interval.slice(1, interval.length - 1);
   const split = inner.split(usesCommaDelimiter ? "," : "..");
 
-  if (!split || split.length !== 2) {
-    res.success = false;
-    return res;
-  }
+  if (!split || split.length !== 2)
+    return setErrorAndType(res, "Either a start or an end character wasn't provided.");
 
   res.from = parseInt(split[0]);
   res.to = parseInt(split[1]);
 
-  if (res.from > res.to) {
-    res.success = false;
-    return res;
-  }
+  if (res.from > res.to) res.reverse = true;
 
   return res;
 }
 
 export function arrayFromInterval(interval) {
-  const { success, from, to, includeEnd, includeStart } = getIntervalInfo(interval);
+  const { success, from, to, includeEnd, includeStart, reverse } = getIntervalInfo(interval);
   if (!success) return [];
 
   let start = includeStart ? from : from + 1;
   let end = includeEnd ? to + 1 : to;
   let array = [];
   for (let i = start; i < end; i++) array.push(i);
-  return array;
+  return reverse ? array.reverse() : array;
 }
