a43b67091f93a9b4e9b986f3feded03ecbb651de
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-17 00:55:36 +0000

    extended mover class to auto kill entity when they reach the end of the path

--- modules/lib/entities/mover.js
+++ modules/lib/entities/mover.js
@@ -51,8 +51,9 @@ export class EntityMover extends Entity {
       this.pos.x = target.pos.x + target.size.x / 2 - this.size.x / 2;
       this.pos.y = target.pos.y + target.size.y / 2 - this.size.y / 2;
       this.currentTarget++;
-      if (this.currentTarget >= this.targets.length && this.targets.length > 1)
-        this.currentTarget = 0;
+      if (this.currentTarget >= this.targets.length)
+        if (this.killOnEndReached) this.kill();
+        else this.currentTarget = 0;
     }
   }
 }
