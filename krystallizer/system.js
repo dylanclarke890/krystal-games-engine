965a5a7b4e12f6563aeb8b87b42abd5c16824c12
dylanclarke890 <dylanclarke890@gmail.com>
2023-03-04 13:27:23 +0000

    refactored system, fixed overflow issue on resize

--- krystallizer/system.js
+++ krystallizer/system.js
@@ -5,28 +5,19 @@ import { InputEvents } from "./enums.js";
 export class System {
   constructor() {
     this.tick = 0;
-
     this.header = document.querySelector("header");
     this.panels = document.querySelector("#panels");
     this.toolbar = document.querySelector("#toolbar");
-    this.height = window.innerHeight - this.header.offsetHeight - 1;
-    this.width = window.innerWidth - this.panels.offsetWidth - this.toolbar.offsetWidth;
-    this.realHeight = this.height;
-    this.realWidth = this.width;
-
     this.canvas = document.querySelector("canvas");
-    this.canvas.height = this.height;
-    this.canvas.width = this.width;
-
     this.ctx = this.canvas.getContext("2d");
     this.ctx.textBaseline = "top";
     this.ctx.font = config.labels.font;
-
     this.scale = 1;
     this.drawPosition = this.DRAW.SMOOTH;
     this.mouseLast = { x: 0, y: 0 };
     this.mouse = { x: 0, y: 0 };
     this.bindEvents();
+    this.resize();
     this.ready = true;
   }
 
@@ -37,8 +28,6 @@ export class System {
 
   /** @param {TouchEvent | MouseEvent} e */
   updateMousePosition(e) {
-
-
     const internalWidth = this.canvas.offsetWidth || this.realWidth;
     const scale = this.scale * (internalWidth / this.realWidth);
 
@@ -54,13 +43,10 @@ export class System {
   }
 
   resize() {
-    const header = document.querySelector("header");
-    const panels = document.querySelector("#panels");
-    this.height = window.innerHeight - header.offsetHeight - 1;
-    this.width = window.innerWidth - panels.offsetWidth;
+    this.height = window.innerHeight - this.header.offsetHeight - 1;
+    this.width = window.innerWidth - this.panels.offsetWidth - this.toolbar.offsetWidth;
     this.realHeight = this.height;
     this.realWidth = this.width;
-    this.canvas = document.querySelector("canvas");
     this.canvas.height = this.height;
     this.canvas.width = this.width;
 
