6cefb1c729dc155aa00bb971198a53ea37193847
dylanclarke890 <dylanclarke890@gmail.com>
2023-03-07 19:18:39 +0000

    added base command pattern as well as some basic classes for the selection box tool

---
+++ krystallizer/tools/undo-commands.js
@@ -0,0 +1,67 @@
+export class Command {
+  execute() {
+    throw new Error("execute() must be implemented");
+  }
+
+  undo() {
+    throw new Error("undo() must be implemented");
+  }
+}
+
+export class CompositeCommand extends Command {
+  /** @type {Command[]} */
+  #commands;
+
+  constructor(commands) {
+    super();
+    this.#commands = commands;
+  }
+
+  execute() {
+    for (let i = 0; i < this.#commands.length; i++) this.#commands[i].execute();
+  }
+
+  undo() {
+    for (let i = this.#commands.length - 1; i >= 0; i--) this.#commands[i].undo();
+  }
+}
+
+//#region SelectionBox Actions
+
+export class SelectionBoxMoveCommand extends Command {
+  constructor(box, dx, dy) {
+    super();
+    /** @type {import("./selection-box.js").SelectionBox} */
+    this.box = box;
+    this.dx = dx;
+    this.dy = dy;
+  }
+
+  execute() {
+    this.box.move(this.dx, this.dy);
+  }
+
+  undo() {
+    this.box.move(-this.dx, -this.dy);
+  }
+}
+
+export class ResizeCommand extends Command {
+  constructor(box, dw, dh) {
+    super();
+    /** @type {import("./selection-box.js").SelectionBox} */
+    this.box = box;
+    this.dw = dw;
+    this.dh = dh;
+  }
+
+  execute() {
+    this.box.resize(this.dw, this.dh);
+  }
+
+  undo() {
+    this.box.resize(-this.dw, -this.dh);
+  }
+}
+
+//#endregion SelectionBox Actions
