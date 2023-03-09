b33d65b44a9142c6523d5c7aef8ad2bb2c2970a7
dylanclarke890 <dylanclarke890@gmail.com>
2023-03-09 16:14:10 +0000

    boilerplate for image scaler class

---
+++ modules/core/assets/image-scaler.js
@@ -0,0 +1,16 @@
+export class ImageScaler {
+  /**
+   * @param {Image} image
+   * @param {number} scale
+   */
+  static resizeImage(image, scale) {}
+
+  /**
+   * @param {Image} image
+   * @param {number} x
+   * @param {number} y
+   * @param {number} width
+   * @param {number} height
+   */
+  static getImagePixels(image, x, y, width, height) {}
+}
