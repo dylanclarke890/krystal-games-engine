64234271deae22153f8b5d31196bd89342c4bf67
dylanclarke890 <dylanclarke890@gmail.com>
2023-03-10 10:16:20 +0000

    added some events

--- modules/core/events.js
+++ modules/core/events.js
@@ -1,1 +1,16 @@
-// TODO
+import { Enum } from "../lib/utils/enum.js";
+
+export class GameEvents extends Enum {
+  //region Input
+  static MouseMove = new GameEvents();
+  static WindowResized = new GameEvents();
+  static KeyPressed = new GameEvents();
+  //endregion Input
+
+  static GameInitialised = new GameEvents();
+  static NextFrame = new GameEvents();
+
+  static {
+    this.freeze();
+  }
+}
