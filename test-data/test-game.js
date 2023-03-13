4d9d45597807b17f021984758ecee23bbed30307
dylanclarke890 <dylanclarke890@gmail.com>
2023-03-13 18:42:42 +0000

    started minimal test game

--- test-data/test-game.js
+++ test-data/test-game.js
@@ -1,31 +1,10 @@
-import { TextAlign } from "../modules/core/assets/font.js";
-import { KrystalGame } from "../modules/core/game.js";
-import { Register } from "../modules/core/register.js";
-import { GameRunner } from "../modules/core/runner.js";
-import { Player } from "./entities/entities.js";
+import { GameLoop } from "../modules/core/time/game-loop.js";
 
-class TestGame extends KrystalGame {
-  constructor(opts) {
-    super(opts);
-    this.spawnEntity(Player, 100, 100);
-  }
-
-  draw() {
-    super.draw();
-    this.fonts.standard.write("Working!", 250, 250, {
-      align: TextAlign.Center,
-      color: "green",
-    });
+class Game {
+  constructor() {
+    this.loop = new GameLoop(60);
+    this.loop.start();
   }
 }
 
-Register.preloadImages("./test-data/assets/entities/mover.png");
-
-new GameRunner({
-  gameClass: TestGame,
-  canvasId: "canvas1",
-  width: 500,
-  height: 500,
-  scale: 1,
-  fonts: { standard: "./test-data/assets/arcade-classic.TTF" },
-});
+new Game();
