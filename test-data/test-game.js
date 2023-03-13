1156f38287ec610338b9401f9d518c1019b96c02
dylanclarke890 <dylanclarke890@gmail.com>
2023-03-13 20:03:42 +0000

    working on test game

--- test-data/test-game.js
+++ test-data/test-game.js
@@ -2,12 +2,16 @@ import { CanvasContext } from "../modules/core/graphics/canvas-context.js";
 import { EventSystem, GameEvents } from "../modules/core/event-system.js";
 import { GameLoop } from "../modules/core/time/game-loop.js";
 
+const moverAssets = "test-data/assets/entities/mover.png";
+
 class Game {
   constructor() {
     this.loop = new GameLoop(60);
     this.ctx = new CanvasContext(500, 500);
     this.tick = 0;
     this.#bindEvents();
+    this.img = new Image();
+    this.img.src = moverAssets;
     this.loop.start();
   }
 
