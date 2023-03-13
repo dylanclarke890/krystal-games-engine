b2441616010485bbc7ccba061ab2e29818e012d6
dylanclarke890 <dylanclarke890@gmail.com>
2023-03-13 19:36:01 +0000

    added canvas context class

--- modules/core/canvas-context.js
+++ modules/core/canvas-context.js
@@ -19,5 +19,26 @@ export class CanvasContext {
     canvas.width = this.width;
     canvas.height = this.height;
     this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+  }
+
+  clear(color) {
+    if (color) {
+      this.ctx.fillStyle = color;
+      this.ctx.fillRect(0, 0, this.width, this.height);
+    } else {
+      this.ctx.clearRect(0, 0, this.width, this.height);
+    }
+  }
+
+  drawRect(x, y, width, height, color) {
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(x, y, width, height);
+  }
+
+  drawText(text, x, y, font, color) {
+    this.ctx.fillStyle = color;
+    this.ctx.font = font;
+    this.ctx.fillText(text, x, y);
   }
 }
