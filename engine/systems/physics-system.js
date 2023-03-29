e9569611550530d3b4c36876aad9a4d0711d2eba
dylanclarke890 <dylanclarke890@gmail.com>
2023-03-29 14:11:23 +0100

    added checks for components before using them

--- engine/systems/physics-system.js
+++ engine/systems/physics-system.js
@@ -1,3 +1,5 @@
+import { Viewport } from "../graphics/viewport.js";
+import { Guard } from "../utils/guard.js";
 import { SystemTypes } from "./system-types.js";
 import { System } from "./system.js";
 
@@ -19,77 +21,91 @@ export class PhysicsSystem extends System {
   }
 }
 
+export class PhysicsSystem2 extends System {
+  static requiredComponents = ["PositionComponent", "VelocityComponent"];
+  static systemType = SystemTypes.Physics;
 
-import {
-  PositionComponent,
-  VelocityComponent,
-  AccelerationComponent,
-  FrictionComponent,
-  SizeComponent,
-  OffsetComponent,
-  GravityFactorComponent,
-  BouncinessComponent,
-  MinBounceVelocityComponent,
-} from "./components.js";
+  viewport;
 
-export class PhysicsSystem2 {
-  constructor(entities) {
-    this.entities = entities;
+  /**
+   *
+   * @param {import("../entities/entity-manager.js").EntityManager} entityManager
+   * @param {Viewport} viewport
+   */
+  constructor(entityManager, viewport) {
+    super(entityManager);
+    Guard.againstNull({ viewport }).isInstanceOf(Viewport);
+    this.viewport = viewport;
   }
 
   update(deltaTime) {
-    for (const entity of this.entities) {
-      const position = entity.getComponent(PositionComponent);
-      const velocity = entity.getComponent(VelocityComponent);
-      const acceleration = entity.getComponent(AccelerationComponent);
-      const friction = entity.getComponent(FrictionComponent);
-      const size = entity.getComponent(SizeComponent);
-      const offset = entity.getComponent(OffsetComponent);
-      const gravityFactor = entity.getComponent(GravityFactorComponent);
-      const bounciness = entity.getComponent(BouncinessComponent);
-      const minBounceVelocity = entity.getComponent(MinBounceVelocityComponent);
+    const entities = this.entityManager.getEntitiesWithComponents(
+      ...PhysicsSystem2.requiredComponents
+    );
+    for (const entity of entities) {
+      const position = entity.getComponent("PositionComponent");
+      const velocity = entity.getComponent("VelocityComponent");
+      const acceleration = entity.getComponent("AccelerationComponent");
+      const friction = entity.getComponent("FrictionComponent");
+      const size = entity.getComponent("SizeComponent");
+      const offset = entity.getComponent("OffsetComponent");
+      const gravityFactor = entity.getComponent("GravityFactorComponent");
+      const bounciness = entity.getComponent("BouncinessComponent");
+      const minBounceVelocity = entity.getComponent("MinBounceVelocityComponent");
 
       // Apply acceleration
-      velocity.x += acceleration.x * deltaTime;
-      velocity.y += acceleration.y * deltaTime;
+      if (acceleration) {
+        velocity.x += acceleration.x * deltaTime;
+        velocity.y += acceleration.y * deltaTime;
+      }
 
       // Apply friction
-      velocity.x *= Math.pow(friction.x, deltaTime);
-      velocity.y *= Math.pow(friction.y, deltaTime);
+      if (friction) {
+        velocity.x *= Math.pow(friction.x, deltaTime);
+        velocity.y *= Math.pow(friction.y, deltaTime);
+      }
 
       // Apply gravity
-      velocity.y += 9.81 * gravityFactor.value * deltaTime;
+      if (gravityFactor) {
+        velocity.y += 9.81 * gravityFactor.value * deltaTime;
+      }
 
       // Apply position
       position.x += velocity.x * deltaTime;
       position.y += velocity.y * deltaTime;
+      
+      const canvasWidth = this.viewport.canvas.width;
 
       // Handle collisions with the walls
       if (position.x + offset.x < 0) {
         position.x = -offset.x;
-        velocity.x = -velocity.x * bounciness.value;
-        if (Math.abs(velocity.x) < minBounceVelocity.value) {
+        // bounciness
+        if (bounciness) velocity.x = -velocity.x * bounciness.value;
+        if (minBounceVelocity && Math.abs(velocity.x) < minBounceVelocity.value) {
           velocity.x = 0;
         }
       }
-      if (position.x + offset.x + size.x > canvas.width) {
-        position.x = canvas.width - size.x - offset.x;
-        velocity.x = -velocity.x * bounciness.value;
-        if (Math.abs(velocity.x) < minBounceVelocity.value) {
+
+      if (position.x + offset.x + size.x > canvasWidth) {
+        position.x = canvasWidth - size.x - offset.x;
+        if (bounciness) velocity.x = -velocity.x * bounciness.value;
+        if (minBounceVelocity && Math.abs(velocity.x) < minBounceVelocity.value) {
           velocity.x = 0;
         }
       }
+
       if (position.y + offset.y < 0) {
         position.y = -offset.y;
-        velocity.y = -velocity.y * bounciness.value;
-        if (Math.abs(velocity.y) < minBounceVelocity.value) {
+        if (bounciness) velocity.y = -velocity.y * bounciness.value;
+        if (minBounceVelocity && Math.abs(velocity.y) < minBounceVelocity.value) {
           velocity.y = 0;
         }
       }
-      if (position.y + offset.y + size.y > canvas.height) {
-        position.y = canvas.height - size.y - offset.y;
-        velocity.y = -velocity.y * bounciness.value;
-        if (Math.abs(velocity.y) < minBounceVelocity.value) {
+
+      if (position.y + offset.y + size.y > canvasWidth) {
+        position.y = canvasWidth - size.y - offset.y;
+        if (bounciness) velocity.y = -velocity.y * bounciness.value;
+        if (minBounceVelocity && Math.abs(velocity.y) < minBounceVelocity.value) {
           velocity.y = 0;
         }
       }
