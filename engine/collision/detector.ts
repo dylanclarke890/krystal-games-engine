d93bc6629837fc7802873082f05ea4e308b1cf5a
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-10 14:09:34 +0100

    feat: detector to ts

---
+++ engine/collision/detector.ts
@@ -0,0 +1,52 @@
+import { Collision } from "../components/collision";
+import { Position } from "../components/position.js";
+import { Size } from "../components/size.js";
+import { EntityManager } from "../entities/entity-manager";
+import { Guard } from "../utils/guard";
+import { PairedSet } from "../utils/paired-set";
+
+export class CollisionDetector {
+  entityManager: EntityManager;
+  pairedSet: PairedSet;
+
+  constructor(entityManager: EntityManager) {
+    Guard.againstNull({ entityManager }).isInstanceOf(EntityManager);
+    this.entityManager = entityManager;
+    this.pairedSet = new PairedSet();
+  }
+
+  /**
+   * @returns An array of entity pairs that have collided.
+   */
+  detect(collidables: [number, Position, Collision][]): PairedSet {
+    const em = this.entityManager;
+    this.pairedSet.clear();
+
+    for (let i = 0; i < collidables.length; i++) {
+      const [a, posA] = collidables[i];
+      const sizeA = em.getComponent(a, "Size")!;
+      for (let j = 0; j < collidables.length; j++) {
+        const [b, posB] = collidables[j];
+        if (a === b) continue;
+        const sizeB = em.getComponent(b, "Size")!;
+        if (this.AABBCollisionCheck(posA, sizeA, posB, sizeB)) {
+          this.pairedSet.add([a, b]);
+        }
+      }
+    }
+
+    return this.pairedSet;
+  }
+
+  /**
+   * Aligned Axis Bounding Box check.
+   */
+  AABBCollisionCheck(posA: Position, sizeA: Size, posB: Position, sizeB: Size): boolean {
+    return (
+      posA.x < posB.x + sizeB.x &&
+      posA.x + sizeA.x > posB.x &&
+      posA.y < posB.y + sizeB.y &&
+      posA.y + sizeA.y > posB.y
+    );
+  }
+}
