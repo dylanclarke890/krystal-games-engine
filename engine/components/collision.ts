964e14f2b6aed1325117f960199717b82e878c84
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-10 14:34:27 +0100

    feat: collision to ts

--- engine/components/collision.ts
+++ engine/components/collision.ts
@@ -12,51 +12,58 @@ export const CollisionResponseFlags = {
   Destroy_Other: 1024,
 };
 
-const ResponseFlagKeys = Object.keys(CollisionResponseFlags).reduce((obj, key) => {
-  obj[CollisionResponseFlags[key]] = key;
-  return obj;
-}, {});
+const ReverseCollisionResponseFlags: { [x: string]: string } = {
+  1: "Ignore",
+  2: "Repel",
+  4: "Bounce",
+  8: "Stick",
+  16: "Slide",
+  32: "Push_Self",
+  64: "Push_Other",
+  126: "Damage_Self",
+  256: "Damage_Other",
+  512: "Destroy_Self",
+  1024: "Destroy_Other",
+};
 
 export class Collision {
-  constructor(responseFlags) {
+  responseFlags: number;
+
+  constructor(responseFlags?: number) {
     this.responseFlags = responseFlags ?? 0;
   }
 
   /**
    * Add a response to the component.
-   * @param {keyof typeof CollisionResponseFlags} response - The response to add.
    */
-  addResponse(response) {
+  addResponse(response: keyof typeof CollisionResponseFlags) {
     this.responseFlags |= CollisionResponseFlags[response];
   }
 
   /**
    * Remove a response from the component.
-   * @param {keyof typeof CollisionResponseFlags} response - The response to remove.
    */
-  removeResponse(response) {
+  removeResponse(response: keyof typeof CollisionResponseFlags) {
     this.responseFlags &= ~CollisionResponseFlags[response];
   }
 
   /**
    * Check if the component has a particular response.
-   * @param {keyof typeof CollisionResponseFlags} response - The response to check for.
-   * @returns {boolean} - True if the component has the response, false otherwise.
    */
-  hasResponse(response) {
+  hasResponse(response: keyof typeof CollisionResponseFlags): boolean {
     return !!(this.responseFlags & CollisionResponseFlags[response]);
   }
 
   /**
    * Get a list of responses currently set.
-   * @returns {string[]}
    */
-  getResponses() {
-    const responses = [];
+  getResponses(): string[] {
+    const responses: string[] = [];
     let flag = 1;
     while (flag <= this.responseFlags) {
       if (flag & this.responseFlags) {
-        responses.push(ResponseFlagKeys[flag]);
+        const key = flag.toString();
+        responses.push(ReverseCollisionResponseFlags[key]);
       }
       flag <<= 1;
     }
