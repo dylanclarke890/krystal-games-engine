fda31e77a3812991f7b548e889f0450419580cd6
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-10 13:39:03 +0100

    feat: converted guard to ts

--- engine/utils/guard.ts
+++ engine/utils/guard.ts
@@ -1,6 +1,6 @@
 /** Helper class for validation. */
 export class Guard {
-  static #getKeyValue(keyValue) {
+  static #getKeyValue(keyValue: any) {
     if (!keyValue) throw new TypeError(`Non object passed: ${keyValue}`);
     const key = Object.keys(keyValue)[0];
     const value = keyValue[key];
@@ -11,13 +11,12 @@ export class Guard {
    * Throws an error if 'keyValue' is null. Variables to validate must be passed wrapped in an object.
    * @example Guard.againstNull({ valueToCheck });
    */
-  static againstNull(keyValue) {
+  static againstNull(keyValue: any) {
     const { key, value } = this.#getKeyValue(keyValue);
     if (value == null) throw new Error(`"${key}" is required.`);
     return {
-      isInstanceOf: (instance) => this.isInstanceOf(keyValue, instance),
-      /** @param {"function" | "object" | "number" | "string" | "boolean" | "undefined" | "bigint" | "symbol"} type */
-      isTypeOf: (type) => this.isTypeOf(keyValue, type),
+      isInstanceOf: (instance: any) => this.isInstanceOf(keyValue, instance),
+      isTypeOf: (type: DataType) => this.isTypeOf(keyValue, type),
     };
   }
 
@@ -27,7 +26,7 @@ export class Guard {
    * @param {"function" | "object" | "number" | "string" | "boolean" | "undefined" | "bigint" | "symbol"} type
    * @example Guard.isTypeOf({ valueToCheck }, "string");
    */
-  static isTypeOf(keyValue, type) {
+  static isTypeOf(keyValue: any, type: DataType) {
     const { key, value } = this.#getKeyValue(keyValue);
     if (typeof value !== type) throw new TypeError(`${key} must be of type "${type}."`);
   }
@@ -37,9 +36,19 @@ export class Guard {
    * wrapped in an object.
    * @example Guard.isInstanceOf({ valueToCheck }, ClassDefinition);
    */
-  static isInstanceOf(keyValue, instanceOf) {
+  static isInstanceOf(keyValue: any, instanceOf: any) {
     const { key, value } = this.#getKeyValue(keyValue);
     if (!(value instanceof instanceOf))
       throw new TypeError(`${key} must be an instance of "${instanceOf.name}".`);
   }
 }
+
+type DataType =
+  | "bigint"
+  | "boolean"
+  | "function"
+  | "number"
+  | "object"
+  | "string"
+  | "symbol"
+  | "undefined";
