7d88955886b1fbdd8a16db321622f83a9af6be21
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-13 15:00:43 +0100

    feat: restarted progess on Assert class

--- engine/utils/assert.ts
+++ engine/utils/assert.ts
@@ -1,48 +1,7 @@
-type Primitive = number | string | undefined | bigint | boolean;
-type TypeOf = "number" | "string" | "boolean" | "object" | "function" | 
-
-typeof "" === "boolean"
 class Assert {
-  static areEqual(expected: Primitive, actual: Primitive, message = "Values are not equal") {
-    if (expected !== actual) {
-      throw new AssertionError(message);
-    }
-  }
-
-  static isTrue(value: boolean, message = "Value is not true"): asserts value is true {
-    if (value !== true) {
-      throw new AssertionError(message);
-    }
-  }
-
-  static isFalse(value: boolean, message = "Value is not false"): asserts value is false {
-    if (value !== false) {
-      throw new AssertionError(message);
-    }
-  }
-
-  static isNull(value: null | unknown, message = "Value is not null"): asserts value is null {
-    if (value !== null) {
-      throw new AssertionError(message);
-    }
-  }
-
-  static isNotNull(value: string, message = "Value is null") {
-    if (value === null) {
-      throw new AssertionError(message);
-    }
-  }
-
-  static isUndefined(value: string, message = "Value is not undefined") {
-    if (value !== undefined) {
-      throw new AssertionError(message);
-    }
-  }
-
-  static isNotUndefined(value: string, message = "Value is undefined") {
-    if (value === undefined) {
-      throw new AssertionError(message);
-    }
+  static isString(value?: unknown, message?: string): asserts value is string {
+    if (typeof value !== "string")
+      throw new AssertionError(message ?? `${value} was not of type 'string'.`);
   }
 }
 
