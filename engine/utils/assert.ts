1f0d07002402619043fefa14c5bd45587ac36ac3
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-13 15:10:50 +0100

    feat: added some more methods to assert

--- engine/utils/assert.ts
+++ engine/utils/assert.ts
@@ -1,7 +1,30 @@
 class Assert {
-  static isString(value?: unknown, message?: string): asserts value is string {
+  static #defaultTypeError<T>(value: T, type: string): string {
+    return `${value} was not of type '${type}'.`;
+  }
+
+  static string(value: unknown, message?: string): asserts value is string {
     if (typeof value !== "string")
-      throw new AssertionError(message ?? `${value} was not of type 'string'.`);
+      throw new AssertionError(message ?? Assert.#defaultTypeError(value, "string"));
+  }
+
+  static number(value: unknown, message?: string): asserts value is number {
+    if (typeof value !== "number")
+      throw new AssertionError(message ?? Assert.#defaultTypeError(value, "number"));
+  }
+
+  static bool(value: unknown, message?: string): asserts value is boolean {
+    if (typeof value !== "boolean")
+      throw new AssertionError(message ?? Assert.#defaultTypeError(value, "boolean"));
+  }
+
+  static instance<T extends Function>(
+    value: unknown,
+    other: T,
+    message?: string
+  ): asserts value is T {
+    if (!(value instanceof other))
+      throw new AssertionError(message ?? `${value} is not an instanceof ${other?.name}`);
   }
 }
 
