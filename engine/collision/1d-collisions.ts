48bd473a0963fb72b466f3e795cad756199a68ea
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-21 13:38:45 +0100

    feat: moving 1d collision logic to separate file

---
+++ engine/collision/1d-collisions.ts
@@ -0,0 +1,32 @@
+export function getNewVelocitiesForElastic(
+  vAi: number,
+  vBi: number,
+  mA: number,
+  mB: number
+): [number, number] {
+  if (mA === mB) {
+    return [vBi, vAi]; // Can just swap velocities if masses are equal
+  }
+
+  // Calculate new velocities using conservation of momentum equation:
+  const totalMass = mA + mB;
+  const vAf = ((mA - mB) * vAi + 2 * mB * vBi) / totalMass;
+  const vBf = (2 * mA * vAi + (mB - mA) * vBi) / totalMass;
+
+  return [vAf, vBf];
+}
+
+export function getNewVelocitiesForPerfectlyInelastic(
+  vAi: number,
+  vBi: number,
+  mA: number,
+  mB: number
+): [number, number] {
+  const fA = mA * vAi;
+  const fB = mB * vBi;
+  const totalMass = mA + mB;
+
+  const newVel = (fA + fB) / totalMass;
+
+  return [newVel, newVel];
+}
