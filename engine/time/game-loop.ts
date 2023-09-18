0e7f1d062dcd9a725e47c9944c962e3aff31e2e5
dylanclarke890 <dylanclarke890@gmail.com>
2023-09-19 00:16:18 +0100

    general tidy up of game loop; add return types, clean up indentatiom

--- engine/time/game-loop.ts
+++ engine/time/game-loop.ts
@@ -9,7 +9,7 @@ export class GameLoop {
   clock: Timer;
   fpsInterval: number;
   targetFps: number;
-  #rafId: number;
+  #requestAnimationFrameId: number;
   stopped: boolean;
 
   constructor(eventSystem: EventSystem, targetFps = 60) {
@@ -19,29 +19,35 @@ export class GameLoop {
     this.targetFps = targetFps;
     this.fpsInterval = 1000 / targetFps;
     this.#lastFrame = -1;
-    this.#rafId = -1;
+    this.#requestAnimationFrameId = -1;
     this.stopped = false;
   }
 
-  start() {
+  start(): void {
     this.stopped = false;
     this.eventSystem.trigger(GameEvents.Loop_BeforeStart);
     this.main(performance.now());
   }
 
-  main(timestamp: number) {
-    if (this.stopped) return cancelAnimationFrame(this.#rafId);
-    this.#rafId = requestAnimationFrame((t) => this.main(t));
+  main(timestamp: number): void {
+    if (this.stopped) {
+      cancelAnimationFrame(this.#requestAnimationFrameId);
+      return;
+    }
 
+    this.#requestAnimationFrameId = requestAnimationFrame((t) => this.main(t));
     Timer.step();
-    const elapsed = timestamp - this.#lastFrame;
-    if (elapsed < this.fpsInterval) return;
-    this.#lastFrame = timestamp - (elapsed % this.fpsInterval);
 
+    const elapsed = timestamp - this.#lastFrame;
+    if (elapsed < this.fpsInterval) {
+      return;
+    }
+
+    this.#lastFrame = timestamp - (elapsed % this.fpsInterval);
     this.eventSystem.trigger(GameEvents.Loop_NextFrame, this.clock.tick());
   }
 
-  stop(unloadAssets?: boolean) {
+  stop(unloadAssets?: boolean): void {
     this.stopped = true;
     this.eventSystem.trigger(GameEvents.Loop_Stop, !!unloadAssets);
   }
