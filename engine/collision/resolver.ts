4ab6b19ca5626a08adf5dd5e1b22855dc6af1b75
dylanclarke890 <dylanclarke890@gmail.com>
2023-09-20 01:24:42 +0100

    fixed errors with resolver

--- engine/collision/resolver.ts
+++ engine/collision/resolver.ts
@@ -1,7 +1,4 @@
-import { Bounciness } from "../components/bounciness.js";
-import { Mass } from "../components/mass.js";
-import { Position } from "../components/position.js";
-import { Size } from "../components/size.js";
+import { Bounciness, Mass, Position, Size } from "../components/index.js";
 import { EntityManager } from "../entities/entity-manager.js";
 import { Viewport } from "../graphics/viewport.js";
 import { Assert } from "../utils/assert.js";
@@ -56,15 +53,20 @@ export class CollisionResolver {
   }
 
   #findSideOfEntityCollision(a: Defined<ResolverComponents>, b: Defined<ResolverComponents>): SideOfCollision {
+    const aHalfX = a.Size.x / 2;
+    const bHalfX = b.Size.x / 2;
+    const aHalfY = a.Size.y / 2;
+    const bHalfY = b.Size.y / 2;
+
     // Get midpoints
-    const aMidX = a.Position.x + a.Size.halfX;
-    const aMidY = a.Position.y + a.Size.halfY;
-    const bMidX = b.Position.x + b.Size.halfX;
-    const bMidY = b.Position.y + b.Size.halfY;
+    const aMidX = a.Position.x + aHalfX;
+    const aMidY = a.Position.y + aHalfY;
+    const bMidX = b.Position.x + bHalfX;
+    const bMidY = b.Position.y + bHalfY;
 
     // Find side of entry based on the normalized sides
-    const dx = (aMidX - bMidX) / (a.Size.halfX + b.Size.halfX);
-    const dy = (aMidY - bMidY) / (a.Size.halfY + b.Size.halfY);
+    const dx = (aMidX - bMidX) / (aHalfX + bHalfX);
+    const dy = (aMidY - bMidY) / (aHalfY + bHalfY);
     const absDX = Math.abs(dx);
     const absDY = Math.abs(dy);
 
