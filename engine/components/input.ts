667bdf8eee3eb360409717d687254a403a280d43
dylanclarke890 <dylanclarke890@gmail.com>
2023-09-21 23:34:32 +0100

    added unbind method to input, made bindings optional

--- engine/components/input.ts
+++ engine/components/input.ts
@@ -3,11 +3,15 @@ import { InputBindings } from "../utils/types.js";
 export class Input {
   actions: Map<string, InputBindings>;
 
-  constructor(bindings: Map<string, InputBindings>) {
-    this.actions = bindings;
+  constructor(bindings?: Map<string, InputBindings>) {
+    this.actions = bindings ?? new Map();
   }
 
   bind(name: string, action: InputBindings) {
     this.actions.set(name, action);
+  }
+
+  unbind(name: string) {
+    this.actions.delete(name);
   }
 }
