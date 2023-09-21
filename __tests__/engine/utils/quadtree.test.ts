f626a0c4aa0d27d53a76ae447ed811c173efb506
dylanclarke890 <dylanclarke890@gmail.com>
2023-09-21 04:15:54 +0100

    managed to get bounds node working, now will rework to be a single class

--- __tests__/engine/utils/quadtree.test.ts
+++ __tests__/engine/utils/quadtree.test.ts
@@ -1,66 +1,70 @@
 import { Vector2D } from "../../../engine/utils/maths/vector-2d";
 import { BoundsNode, PointNode, Quadtree } from "../../../engine/utils/quadtree";
 
-describe("PointNode", () => {
-  it("should insert points correctly", () => {
-    const rootNode = new PointNode(new Vector2D(0, 0), new Vector2D(100, 100), 0, 4);
-    const point1 = new PointNode(new Vector2D(10, 10), new Vector2D(1, 1), 0);
-    const point2 = new PointNode(new Vector2D(20, 20), new Vector2D(1, 1), 0);
-
-    rootNode.insert(point1);
-    rootNode.insert(point2);
-
-    expect(rootNode.children.length).toBe(2);
-  });
-
-  it("should subdivide when max children are reached", () => {
-    const rootNode = new PointNode(new Vector2D(0, 0), new Vector2D(100, 100), 0, 2);
-    const point1 = new PointNode(new Vector2D(10, 10), new Vector2D(1, 1), 0);
-    const point2 = new PointNode(new Vector2D(20, 20), new Vector2D(1, 1), 0);
-    const point3 = new PointNode(new Vector2D(30, 30), new Vector2D(1, 1), 0);
-
-    rootNode.insert(point1);
-    rootNode.insert(point2);
-    rootNode.insert(point3);
-
-    expect(rootNode.nodes.length).toBe(4);
-  });
-
-  // Add more tests for retrieve, clear, and other methods
-});
-
-describe("BoundsNode", () => {
-  // Add tests for BoundsNode similarly to PointNode
-});
-
 describe("Quadtree", () => {
-  it("should insert and retrieve points correctly", () => {
-    const quadtree = new Quadtree(new Vector2D(0, 0), new Vector2D(100, 100), 2, 4, true);
+  it("handles point nodes correctly", () => {
+    // Setup
+    const topLeft = new Vector2D(0, 0);
+    const size = new Vector2D(100, 100);
+    const quadtree = new Quadtree(topLeft, size, { pointQuad: true, maxChildren: 1 });
+
     const point1 = new PointNode(new Vector2D(10, 10), new Vector2D(1, 1), 0);
-    const point2 = new PointNode(new Vector2D(20, 20), new Vector2D(1, 1), 0);
+    const point2 = new PointNode(new Vector2D(90, 90), new Vector2D(1, 1), 0);
 
-    quadtree.insert(point1);
-    quadtree.insert(point2);
+    // Insert points
+    quadtree.insert([point1, point2]);
+    expect(quadtree.size).toBe(2);
 
-    const retrievedPoints = quadtree.retrieve(point1);
+    // Retrieve points
+    const retrieved1 = quadtree.retrieve(point1);
+    const retrieved2 = quadtree.retrieve(point2);
 
-    expect(retrievedPoints.length).toBe(1);
-    expect(retrievedPoints[0]).toStrictEqual(point1);
+    expect(retrieved1).toContain(point1);
+    expect(retrieved2).toContain(point2);
+
+    // Make sure it doesn't return nodes not in the same quadrant
+    expect(retrieved1).not.toContain(point2);
+    expect(retrieved2).not.toContain(point1);
+
+    // clear quadtree
+    quadtree.clear();
+    expect(quadtree.size).toBe(0);
+
+    // Make sure the node isn't there anymore
+    const retrieved3 = quadtree.retrieve(point1);
+    expect(retrieved3).not.toContain(point1);
   });
 
-  it("should insert and retrieve items with bounds correctly", () => {
-    const quadtree = new Quadtree(new Vector2D(0, 0), new Vector2D(100, 100), 2, 4, false);
+  it("handles bounds nodes correctly", () => {
+    // Setup
+    const topLeft = new Vector2D(0, 0);
+    const size = new Vector2D(100, 100);
+    const quadtree = new Quadtree(topLeft, size, { pointQuad: false, maxChildren: 1 });
+
     const bounds1 = new BoundsNode(new Vector2D(10, 10), new Vector2D(10, 10), 0);
-    const bounds2 = new BoundsNode(new Vector2D(30, 30), new Vector2D(10, 10), 0);
+    const bounds2 = new BoundsNode(new Vector2D(90, 90), new Vector2D(10, 10), 0);
 
-    quadtree.insert(bounds1);
-    quadtree.insert(bounds2);
+    // Insert bounds
+    quadtree.insert([bounds1, bounds2]);
+    expect(quadtree.size).toBe(2);
 
-    const retrievedBounds = quadtree.retrieve(bounds1);
+    // Retrieve bounds
+    const retrieved1 = quadtree.retrieve(bounds1);
+    const retrieved2 = quadtree.retrieve(bounds2);
 
-    expect(retrievedBounds.length).toBe(1);
-    expect(retrievedBounds[0]).toStrictEqual(bounds1);
+    expect(retrieved1).toContain(bounds1);
+    expect(retrieved2).toContain(bounds2);
+
+    // Make sure it doesn't return nodes not in the same quadrant
+    expect(retrieved1).not.toContain(bounds2);
+    expect(retrieved2).not.toContain(bounds1);
+
+    // clear quadtree
+    quadtree.clear();
+    expect(quadtree.size).toBe(0);
+
+    // Make sure the node isn't there anymore
+    const retrieved3 = quadtree.retrieve(bounds1);
+    expect(retrieved3).not.toContain(bounds1);
   });
-
-  // Add more tests for clear, max depth, and other quadtree features
 });
--- engine/utils/quadtree.ts
+++ engine/utils/quadtree.ts
@@ -2,11 +2,11 @@ import { Assert } from "./assert.js";
 import { Vector2D } from "./maths/vector-2d.js";
 import { Bounds } from "./types.js";
 
-enum QuadtreeSegments {
-  TOP_LEFT,
-  TOP_RIGHT,
-  BOTTOM_LEFT,
-  BOTTOM_RIGHT,
+enum Quadrant {
+  NorthWest,
+  NorthEast,
+  SouthWest,
+  SouthEast,
 }
 
 interface INode {
@@ -17,7 +17,7 @@ interface INode {
   depth: number;
   maxDepth: number;
 
-  insert(item: INode): void;
+  insert(item: INode | INode[]): void;
   retrieve(item: INode): INode[];
   clear(): void;
 }
@@ -30,66 +30,63 @@ export class PointNode implements INode {
   depth: number;
   maxDepth: number;
 
-  constructor(position: Vector2D, size: Vector2D, depth: number, maxDepth?: number, maxChildren?: number) {
+  constructor(position: Vector2D, size: Vector2D, depth = 0, maxDepth = 4, maxChildren = 4) {
     this.bounds = { position, size };
     this.depth = depth;
-    this.maxChildren = maxChildren ?? 4;
-    this.maxDepth = maxDepth ?? 4;
+    this.maxChildren = maxChildren;
+    this.maxDepth = maxDepth;
     this.nodes = [];
     this.children = [];
   }
 
-  insert(item: PointNode) {
-    if (this.nodes.length > 0) {
-      const index = this.findIndex(item);
-      this.nodes[index].insert(item);
+  insert(node: PointNode | PointNode[]) {
+    if (Array.isArray(node)) {
+      node.forEach((n) => this.insert(n));
       return;
     }
 
-    this.children.push(item);
+    if (this.children.length) {
+      const quadrant = this.findQuadrant(node);
+      this.children[quadrant].insert(node);
+      return;
+    }
 
-    if (this.depth < this.maxDepth && this.children.length > this.maxChildren) {
+    this.nodes.push(node);
+
+    if (this.nodes.length > this.maxChildren && this.depth < this.maxDepth) {
       this.subdivide();
-      this.children.forEach(this.insert, this);
-      this.children.length = 0;
     }
   }
 
-  retrieve(item: PointNode): PointNode[] {
-    if (this.nodes.length > 0) {
-      const index = this.findIndex(item);
-      return this.nodes[index].retrieve(item);
-    }
-    return this.children;
-  }
-
-  findIndex(item: PointNode) {
-    const left = item.bounds.position.x > this.bounds.position.x + this.bounds.size.x / 2 ? false : true;
-    const top = item.bounds.position.y > this.bounds.position.y + this.bounds.position.y / 2 ? false : true;
-
-    if (left) {
-      return top ? QuadtreeSegments.TOP_LEFT : QuadtreeSegments.BOTTOM_LEFT;
+  retrieve(node: PointNode): PointNode[] {
+    // If this node is subdivided
+    if (this.children.length) {
+      const quadrant = this.findQuadrant(node);
+      return this.children[quadrant].retrieve(node);
     }
 
-    return top ? QuadtreeSegments.TOP_RIGHT : QuadtreeSegments.BOTTOM_RIGHT;
+    return this.nodes;
   }
-
-  #newNode = (position: Vector2D, size: Vector2D) =>
-    new PointNode(position, size, this.depth, this.maxDepth, this.maxChildren);
 
   subdivide() {
-    this.depth++;
+    const halfWidth = this.bounds.size.x / 2;
+    const halfHeight = this.bounds.size.y / 2;
 
-    const size = new Vector2D(this.bounds.size.x / 2, this.bounds.size.y / 2);
-    const topLeftPosition = new Vector2D(this.bounds.position.x, this.bounds.position.y);
-    const topRightPosition = topLeftPosition.clone().add(size.x, 0);
-    const bottomLeftPosition = topLeftPosition.clone().add(0, size.y);
-    const bottomRightPosition = topLeftPosition.clone().add(size.x, size.y);
+    // Positions of the children quadrants
+    const positions = [
+      new Vector2D(this.bounds.position.x, this.bounds.position.y), // NW
+      new Vector2D(this.bounds.position.x + halfWidth, this.bounds.position.y), // NE
+      new Vector2D(this.bounds.position.x, this.bounds.position.y + halfHeight), // SW
+      new Vector2D(this.bounds.position.x + halfWidth, this.bounds.position.y + halfHeight), // SE
+    ];
 
-    this.nodes[QuadtreeSegments.TOP_LEFT] = this.#newNode(topLeftPosition, size);
-    this.nodes[QuadtreeSegments.TOP_RIGHT] = this.#newNode(topRightPosition, size);
-    this.nodes[QuadtreeSegments.BOTTOM_LEFT] = this.#newNode(bottomLeftPosition, size);
-    this.nodes[QuadtreeSegments.BOTTOM_RIGHT] = this.#newNode(bottomRightPosition, size);
+    const size = new Vector2D(halfWidth, halfHeight);
+
+    this.children = positions.map((pos) => new PointNode(pos, size, this.depth + 1, this.maxDepth, this.maxChildren));
+
+    const nodes = [...this.nodes];
+    this.nodes.length = 0;
+    this.insert(nodes);
   }
 
   clear() {
@@ -97,14 +94,54 @@ export class PointNode implements INode {
     this.nodes.forEach((n) => n.clear());
     this.nodes.length = 0;
   }
+
+  findQuadrant(node: PointNode): Quadrant {
+    const left = node.bounds.position.x < this.bounds.position.x + this.bounds.size.x / 2;
+    const top = node.bounds.position.y < this.bounds.position.y + this.bounds.size.y / 2;
+
+    if (left) {
+      return top ? Quadrant.NorthWest : Quadrant.SouthWest;
+    }
+
+    return top ? Quadrant.NorthEast : Quadrant.SouthEast;
+  }
 }
 
 export class BoundsNode extends PointNode implements INode {
-  #stuckChildren: BoundsNode[];
+  #overlappingChildren: BoundsNode[];
 
-  constructor(position: Vector2D, size: Vector2D, depth: number, maxDepth?: number, maxChildren?: number) {
+  constructor(position: Vector2D, size: Vector2D, depth = 0, maxDepth = 4, maxChildren = 4) {
     super(position, size, depth, maxDepth, maxChildren);
-    this.#stuckChildren = [];
+    this.#overlappingChildren = [];
+  }
+
+  insert(node: BoundsNode | BoundsNode[]) {
+    if (Array.isArray(node)) {
+      node.forEach((n) => this.insert(n));
+      return;
+    }
+
+    if (this.children.length) {
+      const quadrant = this.findQuadrant(node);
+
+      if (this.isInBounds(node, this.children[quadrant])) {
+        this.children[quadrant].insert(node);
+      } else {
+        this.#overlappingChildren.push(node);
+      }
+      return;
+    }
+
+    super.insert(node);
+  }
+
+  retrieve(item: BoundsNode): PointNode[] {
+    return super.retrieve(item).concat(this.#overlappingChildren);
+  }
+
+  clear() {
+    this.#overlappingChildren.length = 0;
+    super.clear();
   }
 
   isInBounds(a: PointNode, b: PointNode): boolean {
@@ -115,141 +152,45 @@ export class BoundsNode extends PointNode implements INode {
       a.bounds.position.y + a.bounds.size.y <= b.bounds.position.y + b.bounds.size.y
     );
   }
-
-  insert(item: BoundsNode) {
-    if (this.nodes.length) {
-      const index = this.findIndex(item);
-      const node = this.nodes[index];
-
-      if (this.isInBounds(item, node)) {
-        this.nodes[index].insert(item);
-      } else {
-        this.#stuckChildren.push(item);
-      }
-
-      return;
-    }
-
-    this.children.push(item);
-
-    if (this.depth < this.maxDepth && this.children.length > this.maxChildren) {
-      this.subdivide();
-      this.children.forEach((c) => this.insert(c as BoundsNode));
-      this.children.length = 0;
-    }
-  }
-
-  getChildren() {
-    return [...this.children, ...this.#stuckChildren];
-  }
-
-  retrieve(item: BoundsNode) {
-    const found: BoundsNode[] = [];
-
-    if (this.nodes.length > 0) {
-      const index = this.findIndex(item);
-      const node = this.nodes[index];
-
-      if (this.isInBounds(item, node)) {
-        found.push(...(this.nodes[index].retrieve(item) as BoundsNode[]));
-      } else {
-        // Part of the item are overlapping multiple child nodes. For each of the overlapping nodes,
-        // return all containing objects.
-        if (item.bounds.position.x <= this.nodes[QuadtreeSegments.TOP_RIGHT].bounds.position.x) {
-          if (item.bounds.position.y <= this.nodes[QuadtreeSegments.BOTTOM_LEFT].bounds.position.y) {
-            found.push(...(this.nodes[QuadtreeSegments.TOP_LEFT] as BoundsNode).getAllContent());
-          }
-
-          if (
-            item.bounds.position.y + item.bounds.size.y >
-            this.nodes[QuadtreeSegments.BOTTOM_LEFT].bounds.position.y
-          ) {
-            found.push(...(this.nodes[QuadtreeSegments.BOTTOM_LEFT] as BoundsNode).getAllContent());
-          }
-        }
-
-        if (
-          item.bounds.position.x + item.bounds.position.x >
-          this.nodes[QuadtreeSegments.TOP_RIGHT].bounds.position.x
-        ) {
-          if (item.bounds.position.y <= this.nodes[QuadtreeSegments.BOTTOM_RIGHT].bounds.position.y) {
-            found.push(...(this.nodes[QuadtreeSegments.TOP_RIGHT] as BoundsNode).getAllContent());
-          }
-
-          if (
-            item.bounds.position.y + item.bounds.size.y >
-            this.nodes[QuadtreeSegments.BOTTOM_RIGHT].bounds.position.y
-          ) {
-            found.push(...(this.nodes[QuadtreeSegments.BOTTOM_RIGHT] as BoundsNode).getAllContent());
-          }
-        }
-      }
-    }
-
-    found.push(...this.#stuckChildren);
-    found.push(...(this.children as BoundsNode[]));
-
-    return found;
-  }
-
-  getAllContent(): BoundsNode[] {
-    const content: BoundsNode[] = [];
-
-    content.push(...(this.children as BoundsNode[]));
-    content.push(...this.#stuckChildren);
-
-    this.nodes.forEach((node) => {
-      content.push(...(node as BoundsNode).getAllContent());
-    });
-
-    return content;
-  }
-
-  clear() {
-    this.#stuckChildren.length = 0;
-    this.children.length = 0;
-    this.nodes.forEach((n) => n.clear());
-    this.nodes.length = 0;
-  }
 }
 
-/** A 2d spatial subdivision algorithm. */
+/** A 2d spatial subdivision algorithm data structure. */
 export class Quadtree {
-  bounds: Bounds;
-  /** The maximum number of levels that the quadtree will create. Default is 4. */
-  maxDepth: number;
-  /** The maximum number of children that a node can contain before it is split into sub-nodes. Default is 4. */
-  maxChildren: number;
-
   /** The node representing the entire viewport/bounds. */
   root: INode;
+  size: number;
 
   /**
-   * @param {boolean} pointQuad Whether the QuadTree will contain points, or items with bounds. Default value is false.
+   * @param position Vector with x, y properties representing the top left position of the bounds.
+   * @param size Vector with x, y properties representing the size of the bounds.
+   * @param maxDepth The maximum number of levels that the quadtree will create. Default is 4.
+   * @param maxChildren The maximum number of items per quadrant before subdividing. Default is 4.
+   * @param pointQuad Whether the QuadTree will contain points, or items with bounds. Default value is false.
    **/
-  constructor(position: Vector2D, size: Vector2D, maxDepth: number, maxChildren: number, pointQuad?: boolean) {
+  constructor(position: Vector2D, size: Vector2D, { maxDepth = 4, maxChildren = 4, pointQuad = false } = {}) {
     Assert.instanceOf("position", position, Vector2D);
     Assert.instanceOf("size", size, Vector2D);
-    Assert.number("maxDepth", maxDepth);
-    Assert.number("size", maxChildren);
 
-    this.bounds = { position, size };
-    this.maxDepth = maxDepth;
-    this.maxChildren = maxChildren;
-
+    this.size = 0;
     this.root = new (pointQuad ? PointNode : BoundsNode)(position, size, 0, maxDepth, maxChildren);
   }
 
   insert(value: INode | INode[]) {
-    if (Array.isArray(value)) value.forEach((item) => this.root.insert(item));
-    else this.root.insert(value);
+    if (Array.isArray(value)) {
+      this.size += value.length;
+      value.forEach((item) => this.root.insert(item));
+    } else {
+      this.size++;
+      this.root.insert(value);
+    }
   }
 
   clear() {
     this.root.clear();
+    this.size = 0;
   }
 
   retrieve(value: INode) {
-    return [...this.root.retrieve(value)];
+    return this.root.retrieve(value);
   }
 }
