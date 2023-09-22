d7c06517a518b0071bfa664f5eef10ee98c56a25
dylanclarke890 <dylanclarke890@gmail.com>
2023-09-22 12:58:12 +0100

    added create method to bitwise flags to create flags from an object or enum

--- engine/utils/bitwise-flags.ts
+++ engine/utils/bitwise-flags.ts
@@ -1,4 +1,24 @@
+import { Enum } from "./enum.js";
+
 export class BitwiseFlags<TFlags extends Record<string, number>> {
+  static create<T extends Record<string, number> | typeof Enum>(obj: T): BitwiseFlags<Record<string, number>> {
+    const instance = new BitwiseFlags<Record<string, number>>();
+
+    if (obj.prototype instanceof Enum) {
+      for (const enumInstance of (obj as typeof Enum).enumValues) {
+        instance.add(enumInstance.valueOf());
+      }
+    } else {
+      for (const key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+          instance.add(obj[key] as number);
+        }
+      }
+    }
+
+    return instance;
+  }
+
   flags: number;
 
   constructor() {
