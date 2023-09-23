9890514090b3ec291ee1c0872a338733dbdc471c
dylanclarke890 <dylanclarke890@gmail.com>
2023-09-23 08:28:53 +0100

    added tests for collision strategies

---
+++ __tests__/engine/collision/strategies.test.ts
@@ -0,0 +1,61 @@
+import { pointVsPoint, pointVsRect, rectVsRect } from "../../../engine/collision/strategies";
+import { Vector2D } from "../../../engine/utils/maths/vector-2d.js";
+
+describe("pointVsPoint", () => {
+  it("should return true for points in the same location", () => {
+    const a = new Vector2D(50, 50);
+    const b = new Vector2D(50, 50);
+    expect(pointVsPoint(a, b)).toBe(true);
+  });
+  it("should return false for points in different locations", () => {
+    const a = new Vector2D(50, 50);
+    const b = new Vector2D(50, 51);
+    expect(pointVsPoint(a, b)).toBe(false);
+  });
+});
+
+describe("pointVsRect", () => {
+  it("should return true for points within the rectangle", () => {
+    const rectPos = new Vector2D(50, 50);
+    const rectSize = new Vector2D(50, 50);
+
+    [new Vector2D(50, 50), new Vector2D(75, 75), new Vector2D(99, 99)].forEach((point) => {
+      expect(pointVsRect(point, rectPos, rectSize)).toBe(true);
+    });
+  });
+  it("should return false for points outside the rectangle", () => {
+    const rectPos = new Vector2D(50, 50);
+    const rectSize = new Vector2D(50, 50);
+
+    [new Vector2D(49, 50), new Vector2D(100, 101)].forEach((point) => {
+      expect(pointVsRect(point, rectPos, rectSize)).toBe(false);
+    });
+  });
+});
+
+describe("rectVsRect", () => {
+  it("should return true for overlapping rectangles", () => {
+    const a = {
+      pos: new Vector2D(25, 25),
+      size: new Vector2D(50, 50),
+    };
+    const b = {
+      pos: new Vector2D(50, 50),
+      size: new Vector2D(50, 50),
+    };
+
+    expect(rectVsRect(a.pos, a.size, b.pos, b.size)).toBe(true);
+  });
+  it("should return false for rectangles not overlapping", () => {
+    const a = {
+      pos: new Vector2D(50, 50),
+      size: new Vector2D(50, 50),
+    };
+    const b = {
+      pos: new Vector2D(0, 0),
+      size: new Vector2D(50, 50),
+    };
+
+    expect(rectVsRect(a.pos, a.size, b.pos, b.size)).toBe(false);
+  });
+});
--- engine/collision/strategies.ts
+++ engine/collision/strategies.ts
@@ -1,17 +1,20 @@
 import { Vector2D } from "../utils/maths/vector-2d.js";
 
-/** Aligned Axis Bounding Box collision check. */
+export function pointVsPoint(a: Vector2D, b: Vector2D) {
+  return a.x === b.x && a.y === b.y;
+}
+
+export function pointVsRect(point: Vector2D, rectPos: Vector2D, rectSize: Vector2D): boolean {
+  return (
+    point.x >= rectPos.x && point.x < rectPos.x + rectSize.x && point.y >= rectPos.y && point.y < rectPos.y + rectSize.y
+  );
+}
+
 export function rectVsRect(rectPosA: Vector2D, rectSizeA: Vector2D, rectPosB: Vector2D, rectSizeB: Vector2D): boolean {
   return (
     rectPosA.x < rectPosB.x + rectSizeB.x &&
     rectPosA.x + rectSizeA.x > rectPosB.x &&
     rectPosA.y < rectPosB.y + rectSizeB.y &&
     rectPosA.y + rectSizeA.y > rectPosB.y
-  );
-}
-
-export function pointVsRect(point: Vector2D, rectPos: Vector2D, rectSize: Vector2D): boolean {
-  return (
-    point.x >= rectPos.x && point.x < rectPos.x + rectSize.x && point.y >= rectPos.y && point.y < rectPos.y + rectSize.y
   );
 }
