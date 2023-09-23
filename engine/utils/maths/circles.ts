de39747594d83911fcb0fe7a5882e8412812774e
dylanclarke890 <dylanclarke890@gmail.com>
2023-09-23 09:02:34 +0100

    added toDegrees and toRadians functions

---
+++ engine/utils/maths/circles.ts
@@ -0,0 +1,6 @@
+export function toDegrees(radians: number): number {
+  return (radians / Math.PI) * 180;
+}
+export function toRadians(degrees: number): number {
+  return (degrees / 180) * Math.PI;
+}
