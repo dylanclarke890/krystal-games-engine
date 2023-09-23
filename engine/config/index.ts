b5bf57993bf23857987bd073800e085a5f4c3c8e
dylanclarke890 <dylanclarke890@gmail.com>
2023-09-23 07:18:29 +0100

    replaced use of config with config manager

---
+++ engine/config/index.ts
@@ -0,0 +1,2 @@
+export { config } from "./config.js";
+export { ConfigManager } from "./config-manager.js";
--- engine/game.ts
+++ engine/game.ts
@@ -7,15 +7,18 @@ import { CollisionDetector } from "./collision/detector.js";
 import { CollisionResolver } from "./collision/resolver.js";
 import { InputSystem, PhysicSystem, RenderSystem, SystemManager } from "./systems/index.js";
 import { EntityQuadtree } from "./entities/entity-quadtree.js";
-import { config } from "./config/config.js";
+import { ConfigManager, config } from "./config/index.js";
 
 export class Game {
   viewport: Viewport;
   eventSystem: EventSystem;
+
   entityManager: EntityManager;
   systemManager: SystemManager;
   inputManager: InputManager;
-  loop: GameLoop;
+  configManager: ConfigManager<typeof config>;
+
+  loop!: GameLoop;
 
   /**
    * @param {string?} canvasId Id of the canvas, if known.
@@ -25,24 +28,28 @@ export class Game {
   constructor(canvasId: string | undefined, width: number, height: number) {
     this.viewport = new Viewport(width, height, canvasId);
     this.eventSystem = new EventSystem();
+    this.configManager = new ConfigManager(config);
     this.entityManager = new EntityManager(this.eventSystem);
     this.systemManager = new SystemManager(this.eventSystem, this.entityManager);
     this.inputManager = new InputManager(this.eventSystem, this.viewport);
-    this.loop = new GameLoop(this.eventSystem, config.frameRate);
     this.setup();
   }
 
   setup() {
     const entityManager = this.entityManager;
     const eventSystem = this.eventSystem;
+    const configManager = this.configManager;
+    const systemManager = this.systemManager;
 
-    const quadtree = new EntityQuadtree(this.viewport, { maxDepth: config.quadtreeMaxDepth });
+    const quadtree = new EntityQuadtree(this.viewport, { maxDepth: configManager.getInt("quadtreeMaxDepth") });
     const detector = new CollisionDetector(entityManager, this.viewport, quadtree);
     const resolver = new CollisionResolver(entityManager, this.viewport);
 
-    this.systemManager.registerSystem(new InputSystem(entityManager, eventSystem, this.inputManager));
-    this.systemManager.registerSystem(new PhysicSystem(entityManager, eventSystem, quadtree, detector, resolver));
-    this.systemManager.registerSystem(new RenderSystem(entityManager, eventSystem, this.viewport));
+    systemManager.registerSystem(new InputSystem(entityManager, eventSystem, this.inputManager));
+    systemManager.registerSystem(new PhysicSystem(entityManager, eventSystem, quadtree, detector, resolver));
+    systemManager.registerSystem(new RenderSystem(entityManager, eventSystem, this.viewport));
+
+    this.loop = new GameLoop(this.eventSystem, this.configManager.getInt("frameRate") ?? 60);
   }
 
   start() {
