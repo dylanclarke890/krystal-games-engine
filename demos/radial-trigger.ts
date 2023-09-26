47ac385f1ac1427f3f83dbddd996c25f755ca47b
dylanclarke890 <dylanclarke890@gmail.com>
2023-09-26 21:39:58 +0100

    boilerplate for radial trigger

---
+++ demos/radial-trigger.ts
@@ -0,0 +1,18 @@
+import { Position, Size } from "../engine/components/2d/index.js";
+import { GameEvents } from "../engine/constants/enums.js";
+import { KrystalGameEngine } from "../engine/main/engine.js";
+
+export class RadialTriggerDemo extends KrystalGameEngine {
+  radialTriggerId: number;
+  constructor() {
+    super("canvas1", 500, 500);
+    this.eventSystem.on(GameEvents.Loop_NextFrame, () => this.update());
+    this.inputManager.enableMouse();
+
+    this.radialTriggerId = this.entityManager.createEntity();
+    this.entityManager.addComponent(this.radialTriggerId, new Position(225, 225));
+    this.entityManager.addComponent(this.radialTriggerId, new Size(5));
+  }
+
+  update() {}
+}
