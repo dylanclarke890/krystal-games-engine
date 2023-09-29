a6728b33081b2b771b9f8f3f545820146cce0ad4
dylanclarke890 <dylanclarke890@gmail.com>
2023-09-29 06:26:03 +0100

    moved entity manager

--- engine/engine.ts
+++ engine/engine.ts
@@ -1,6 +1,6 @@
 import { config, GameConfig } from "./config.js";
 import { CollisionDetector, CollisionResolver } from "./collision/index.js";
-import { EntityManager } from "./entities/entity-manager.js";
+import { EntityManager } from "./managers/entity-manager.js";
 import { Quadtree } from "./collision/broadphase/quadtree.js";
 import { EventSystem } from "./events/event-system.js";
 import { Viewport } from "./graphics/viewport.js";
