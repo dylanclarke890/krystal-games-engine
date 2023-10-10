5a68c0164405343ea3a4b8c289bfea86366b125e
dylanclarke890 <dylanclarke890@gmail.com>
2023-10-10 07:55:01 +0100

    remove event listener from base integrator class

--- engine/physics/integrators/base-integrator.ts
+++ engine/physics/integrators/base-integrator.ts
@@ -1,5 +1,4 @@
 import { RigidBody } from "../../components/rigid-body.js";
-import { GameEvents } from "../../constants/enums.js";
 import { GameContext } from "../../core/context.js";
 import { Vector2 } from "../../maths/vector2.js";
 import { IObjectPool } from "../../types/common-interfaces.js";
@@ -19,7 +18,6 @@ export abstract class BaseIntegrator {
   constructor(context: GameContext) {
     this.context = context;
     this.vectorPool = context.objectPools.create("vector2", Vector2, (vec, x, y) => vec.set(x ?? 0, y ?? 0));
-    this.context.events.on(GameEvents.VIEWPORT_COLLISION, this.bounceOffViewportBoundaries.bind(this));
     this.adjustmentBuffer = context.config.getInt("collisionAdjustmentBuffer") ?? 0.1;
     this.pooledVectors = [];
   }
