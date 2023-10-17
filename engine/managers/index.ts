af9f7d5cd22ea62247d3cfa9a4ab865a4e90d9dd
dylanclarke890 <dylanclarke890@gmail.com>
2023-10-17 10:13:58 +0100

    removed unused file

--- engine/managers/index.ts
+++ engine/managers/index.ts
@@ -3,5 +3,4 @@ export * from "./entity-manager.js";
 export * from "./event-manager.js";
 export * from "./input-manager.js";
 export * from "./object-pool-manager.js";
-export * from "./rigid-body-manager.js";
 export * from "./system-manager.js";
