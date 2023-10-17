aa1b99e0d90dc0d3f0ec20b98cbce03361f0a514
dylanclarke890 <dylanclarke890@gmail.com>
2023-10-17 11:41:31 +0100

    remove unused types

--- engine/types/common-types.ts
+++ engine/types/common-types.ts
@@ -9,16 +9,10 @@ import {
   Transform,
   Animation,
 } from "../components/index.js";
-import { Vector2 } from "../maths/vector2.js";
-
-export type Bounds = { position: Vector2; size: Vector2 };
 
 export type InputAction = "held" | "pressed" | "released";
 export type InputActionStatus = { [K in InputAction]: boolean };
 export type InputStatus = InputActionStatus & { locked: boolean };
-
-export type CollisionLayer = "DEFAULT" | "PLAYER" | "ENEMY";
-export type Collidable = [number, RigidBody, Collider];
 
 export type ComponentType =
   | "renderable"
