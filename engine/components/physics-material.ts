0132e19cef0ff6d78bf5ef4e6744290728c56f35
dylanclarke890 <dylanclarke890@gmail.com>
2023-10-17 13:23:11 +0100

    added impulse based resolution

--- engine/components/physics-material.ts
+++ engine/components/physics-material.ts
@@ -1,9 +1,9 @@
 export class PhysicsMaterial {
-  bounciness: number;
+  restitution: number;
   friction: number;
 
   constructor(bounciness?: number, friction?: number) {
-    this.bounciness = bounciness ?? 1;
+    this.restitution = bounciness ?? 1;
     this.friction = friction ?? 0;
   }
 }
--- engine/physics/collision/resolution/resolution-strategies.ts
+++ engine/physics/collision/resolution/resolution-strategies.ts
@@ -2,6 +2,29 @@ import { Vector2 } from "../../../maths/vector2.js";
 import { CollisionInfo } from "../data.js";
 
 export class ResolutionStrategies {
+  static resolveWithImpulses(info: CollisionInfo): void {
+    const a = info.entityA;
+    const b = info.entityB;
+    const aRigidBody = a.collider.rigidBody;
+    const bRigidBody = b.collider.rigidBody;
+
+    if (typeof aRigidBody === "undefined" || typeof bRigidBody === "undefined") {
+      return;
+    }
+
+    const totalInverseMass = 1 / aRigidBody.mass + 1 / bRigidBody.mass;
+    const restitution = Math.min(a.collider.material.restitution, b.collider.material.restitution);
+    const relativeVelocity = aRigidBody.velocity.clone().sub(bRigidBody.velocity);
+
+    const impulseMagnitude = (-(1 + restitution) * relativeVelocity.dot(info.normal)) / totalInverseMass;
+    const impulseDirection = info.normal.clone();
+    const impulse = impulseDirection.mulScalar(impulseMagnitude);
+
+    // Apply impulse to each entity's velocity based on its mass
+    aRigidBody.velocity.add(impulse.clone().divScalar(aRigidBody.mass));
+    bRigidBody.velocity.sub(impulse.clone().divScalar(bRigidBody.mass));
+  }
+
   static resolveCircles(info: CollisionInfo): void {
     const a = info.entityA;
     const b = info.entityB;
@@ -16,8 +39,8 @@ export class ResolutionStrategies {
     const bMass = bRigidBody.mass;
     const totalMass = aMass + bMass;
 
-    const aBounciness = a.collider.material.bounciness;
-    const bBounciness = b.collider.material.bounciness;
+    const aBounciness = a.collider.material.restitution;
+    const bBounciness = b.collider.material.restitution;
     const restitution = Math.min(aBounciness, bBounciness);
 
     const aFriction = a.collider.material.friction;
