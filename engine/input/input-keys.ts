1be61cc8566fe58d683ec1b4ab3b949fff4a02ef
dylanclarke890 <dylanclarke890@gmail.com>
2023-10-17 10:13:25 +0100

    converted input key to typescript enum

--- engine/input/input-keys.ts
+++ engine/input/input-keys.ts
@@ -1,172 +1,166 @@
-import { Enum } from "../utils/enum.js";
-
-export class InputKey extends Enum {
+export enum InputKey {
   //#region Mouse
-  static Mouse_BtnOne = new InputKey();
-  static Mouse_BtnTwo = new InputKey();
-  static Context_Menu = new InputKey();
-  static Mouse_WheelUp = new InputKey();
-  static Mouse_WheelDown = new InputKey();
-  static Mouse_Move = new InputKey();
+  Mouse_BtnOne,
+  Mouse_BtnTwo,
+  Context_Menu,
+  Mouse_WheelUp,
+  Mouse_WheelDown,
+  Mouse_Move,
   //#endregion Mouse
   //#region Touch Device
-  static Touch_Start = new InputKey();
-  static Touch_End = new InputKey();
+  Touch_Start,
+  Touch_End,
   //#endregion Touch Device
 
   //#region Keyboard
-  static Escape = new InputKey();
-  static BackSpace = new InputKey();
-  static Space = new InputKey();
-  static Enter = new InputKey();
-  static Tab = new InputKey();
-  static Shift = new InputKey();
-  static Shift_Left = new InputKey();
-  static Shift_Right = new InputKey();
-  static Ctrl = new InputKey();
-  static Ctrl_Left = new InputKey();
-  static Ctrl_Right = new InputKey();
-  static Alt = new InputKey();
-  static Alt_Left = new InputKey();
-  static Alt_Right = new InputKey();
+  Escape,
+  BackSpace,
+  Space,
+  Enter,
+  Tab,
+  Shift,
+  Shift_Left,
+  Shift_Right,
+  Ctrl,
+  Ctrl_Left,
+  Ctrl_Right,
+  Alt,
+  Alt_Left,
+  Alt_Right,
 
-  static Greater_Than = new InputKey();
-  static Less_Than = new InputKey();
-  static Comma = new InputKey();
-  static Period = new InputKey();
-  static Tilde = new InputKey();
-  static GraveAccent = new InputKey();
-  static Question = new InputKey();
-  static Exclamation = new InputKey();
-  static AtSign = new InputKey();
-  static Hash = new InputKey();
-  static Dollar = new InputKey();
-  static Percent = new InputKey();
-  static Caret = new InputKey();
-  static Pipe = new InputKey();
-  static Ampersand = new InputKey();
-  static Colon = new InputKey();
-  static Colon_Semi = new InputKey();
-  static Slash_Backward = new InputKey();
-  static Slash_Forward = new InputKey();
-  static Bracket_Round_Left = new InputKey();
-  static Bracket_Round_Right = new InputKey();
-  static Bracket_Square_Left = new InputKey();
-  static Bracket_Square_Right = new InputKey();
-  static Bracket_Curly_Left = new InputKey();
-  static Bracket_Curly_Right = new InputKey();
-  static Quote_Single = new InputKey();
-  static Quote_Double = new InputKey();
+  Greater_Than,
+  Less_Than,
+  Comma,
+  Period,
+  Tilde,
+  GraveAccent,
+  Question,
+  Exclamation,
+  AtSign,
+  Hash,
+  Dollar,
+  Percent,
+  Caret,
+  Pipe,
+  Ampersand,
+  Colon,
+  Colon_Semi,
+  Slash_Backward,
+  Slash_Forward,
+  Bracket_Round_Left,
+  Bracket_Round_Right,
+  Bracket_Square_Left,
+  Bracket_Square_Right,
+  Bracket_Curly_Left,
+  Bracket_Curly_Right,
+  Quote_Single,
+  Quote_Double,
 
-  static Home = new InputKey();
-  static End = new InputKey();
-  static Page_Up = new InputKey();
-  static Page_Down = new InputKey();
-  static Pause = new InputKey();
+  Home,
+  End,
+  Page_Up,
+  Page_Down,
+  Pause,
   // Doesn't have a keydown event but has keyup
-  static PrintScreen = new InputKey();
-  static Insert = new InputKey();
-  static Delete = new InputKey();
+  PrintScreen,
+  Insert,
+  Delete,
 
-  static Multiply = new InputKey();
-  static Plus = new InputKey();
-  static Minus = new InputKey();
-  static UnderScore = new InputKey();
+  Multiply,
+  Plus,
+  Minus,
+  UnderScore,
 
-  static Caps_Lock = new InputKey();
-  static Scroll_Lock = new InputKey();
-  static Num_Lock = new InputKey();
-  static Meta = new InputKey();
+  Caps_Lock,
+  Scroll_Lock,
+  Num_Lock,
+  Meta,
 
-  static Arrow_Left = new InputKey();
-  static Arrow_Right = new InputKey();
-  static Arrow_Up = new InputKey();
-  static Arrow_Down = new InputKey();
+  Arrow_Left,
+  Arrow_Right,
+  Arrow_Up,
+  Arrow_Down,
 
-  static A = new InputKey();
-  static B = new InputKey();
-  static C = new InputKey();
-  static D = new InputKey();
-  static E = new InputKey();
-  static F = new InputKey();
-  static G = new InputKey();
-  static H = new InputKey();
-  static I = new InputKey();
-  static J = new InputKey();
-  static K = new InputKey();
-  static L = new InputKey();
-  static M = new InputKey();
-  static N = new InputKey();
-  static O = new InputKey();
-  static P = new InputKey();
-  static Q = new InputKey();
-  static R = new InputKey();
-  static S = new InputKey();
-  static T = new InputKey();
-  static U = new InputKey();
-  static V = new InputKey();
-  static W = new InputKey();
-  static X = new InputKey();
-  static Y = new InputKey();
-  static Z = new InputKey();
-  static Zero = new InputKey();
-  static One = new InputKey();
-  static Two = new InputKey();
-  static Three = new InputKey();
-  static Four = new InputKey();
-  static Five = new InputKey();
-  static Six = new InputKey();
-  static Seven = new InputKey();
-  static Eight = new InputKey();
-  static Nine = new InputKey();
+  A,
+  B,
+  C,
+  D,
+  E,
+  F,
+  G,
+  H,
+  I,
+  J,
+  K,
+  L,
+  M,
+  N,
+  O,
+  P,
+  Q,
+  R,
+  S,
+  T,
+  U,
+  V,
+  W,
+  X,
+  Y,
+  Z,
+  Zero,
+  One,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
 
-  static F1 = new InputKey();
-  static F2 = new InputKey();
-  static F3 = new InputKey();
-  static F4 = new InputKey();
-  static F5 = new InputKey();
-  static F6 = new InputKey();
-  static F7 = new InputKey();
-  static F8 = new InputKey();
-  static F9 = new InputKey();
-  static F10 = new InputKey();
-  static F11 = new InputKey();
-  static F12 = new InputKey();
+  F1,
+  F2,
+  F3,
+  F4,
+  F5,
+  F6,
+  F7,
+  F8,
+  F9,
+  F10,
+  F11,
+  F12,
   //#endregion Keyboard
 
   //#region Accelerometer
-  static Device_Motion = new InputKey();
+  Device_Motion,
   //#endregion Accelerometer
 
   //#region Gamepad
-  static Button_A = new InputKey();
-  static Button_B = new InputKey();
-  static Button_X = new InputKey();
-  static Button_Y = new InputKey();
-  static Button_LB = new InputKey();
-  static Button_RB = new InputKey();
-  static Button_LT = new InputKey();
-  static Button_RT = new InputKey();
-  static Button_Back = new InputKey();
-  static Button_Start = new InputKey();
+  Button_A,
+  Button_B,
+  Button_X,
+  Button_Y,
+  Button_LB,
+  Button_RB,
+  Button_LT,
+  Button_RT,
+  Button_Back,
+  Button_Start,
 
-  static Stick_Left = new InputKey();
-  static Stick_Right = new InputKey();
+  Stick_Left,
+  Stick_Right,
 
-  static Dpad_Up = new InputKey();
-  static Dpad_Down = new InputKey();
-  static Dpad_Left = new InputKey();
-  static Dpad_Right = new InputKey();
+  Dpad_Up,
+  Dpad_Down,
+  Dpad_Left,
+  Dpad_Right,
 
-  static Axis_Left_X = new InputKey();
-  static Axis_Left_Y = new InputKey();
-  static Axis_Right_X = new InputKey();
-  static Axis_Right_Y = new InputKey();
-  static Axis_LT = new InputKey();
-  static Axis_RT = new InputKey();
+  Axis_Left_X,
+  Axis_Left_Y,
+  Axis_Right_X,
+  Axis_Right_Y,
+  Axis_LT,
+  Axis_RT,
   //#endregion Gamepad
-
-  static {
-    this.freeze();
-  }
 }
