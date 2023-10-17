768b268d6d8dd8a0d9a73bb2fc90a34315a748cb
dylanclarke890 <dylanclarke890@gmail.com>
2023-10-17 11:42:24 +0100

    unused files

--- engine/maths/circles.ts
+++
@@ -1,6 +0,0 @@
-export function toDegrees(radians: number): number {
-  return (radians / Math.PI) * 180;
-}
-export function toRadians(degrees: number): number {
-  return (degrees / 180) * Math.PI;
-}
--- engine/maths/triangles.ts
+++
@@ -1,9 +0,0 @@
-export function calcHypotenuse(x: number, y: number, z?: number): number {
-  z ??= 0;
-  return Math.sqrt(x * x + y * y + z * z);
-}
-
-export function calcHypotenuseSquared(x: number, y: number, z?: number): number {
-  z ??= 0;
-  return x * x + y * y + z * z;
-}
